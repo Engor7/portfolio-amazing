e2a7c6a64a4635ffa9ea62d50a1c2f1aae67e4d2
egor <en.gor7@gmail.com>
2026-03-27 18:23:06 +0300

    update ui-library page

--- src/app/projects/ui-library/page.tsx
+++ src/app/projects/ui-library/page.tsx
@@ -13,11 +13,13 @@ import FitnessTracker from "./_components/FitnessTracker/FitnessTracker";
 import GalleryUsers from "./_components/GalleryUsers/GalleryUsers";
 import LyricCard from "./_components/LyricCard/LyricCard";
 import Schedule from "./_components/Schedule/Schedule";
+import SpoilerRevealDemo from "./_components/SpoilerRevealDemo/SpoilerRevealDemo";
 import StockInfo from "./_components/StockInfo/StockInfo";
 import TodoList from "./_components/TodoList/TodoList";
 import ToggleSwitch from "./_components/ToggleSwitch/ToggleSwitch";
 import WeatherForecast from "./_components/WeatherForecast/WeatherForecast";
 import WordPopup from "./_components/WordPopup/WordPopup";
+import { ArrowUpIcon, HomeIcon } from "./icons";
 import styles from "./ui-library.module.scss";
 
 const UILibraryPage = () => {
@@ -52,16 +54,7 @@ const UILibraryPage = () => {
          >
             <div className={styles.toolbarInner}>
                <Link href="/" className={styles.homeBtn}>
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24"
-                     aria-hidden="true"
-                  >
-                     <path
-                        fill="currentColor"
-                        d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1"
-                     />
-                  </svg>
+                  <HomeIcon />
                   <span>Go Home</span>
                </Link>
                {scrolled && (
@@ -73,16 +66,7 @@ const UILibraryPage = () => {
                      }
                      aria-label="Scroll to top"
                   >
-                     <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                     >
-                        <path
-                           fill="currentColor"
-                           d="m11 8.8l-2.9 2.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7t-.7.275t-.7-.275L13 8.8V17q0 .425-.288.713T12 18t-.712-.288T11 17z"
-                        />
-                     </svg>
+                     <ArrowUpIcon />
                      <span>Up</span>
                   </button>
                )}
@@ -104,6 +88,9 @@ const UILibraryPage = () => {
             </div>
             <div className={styles.cell}>
                <WordPopup />
+            </div>
+            <div className={styles.cell}>
+               <SpoilerRevealDemo />
             </div>
             <div className={styles.cell}>
                <CardStack />
@@ -141,7 +128,6 @@ const UILibraryPage = () => {
             <div className={styles.cell}>
                <LyricCard />
             </div>
-            <div className={styles.emptyCell} />
          </div>
 
          <p className={styles.footer}>
