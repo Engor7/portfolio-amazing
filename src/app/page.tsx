73d9cb82fd101efc2552ea413ba48b8153fb10e3
egor <en.gor7@gmail.com>
2026-03-30 17:23:27 +0300

    add experiments to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,29 @@ const Home = () => {
                <span aria-hidden="true">/</span>
                <Link href="/projects/ui-library">Ui</Link>
                <span aria-hidden="true">/</span>
-               <Link href="/projects/sites/anima-one">Anima One</Link>
-               <span aria-hidden="true">/</span>
                <Link href="/projects/sites/view-scroll">View Scroll</Link>
                <span aria-hidden="true">/</span>
                <Link href="/projects/sites/coin">Coin</Link>
                <span aria-hidden="true">/</span>
                <Link href="/projects/sites/spotlight">Spotlight</Link>
-
                <span>/</span>
                <span>[</span>
                <ThemeToggle />
                <span>]</span>
             </nav>
+            <ul className="main-nav main-nav_new">
+               <a href="/projects/exp/scroll-reveal">Scroll Reveal</a>
+               <span aria-hidden="true">/</span>
+               <Link href="/projects/exp/chrome-abyss">Chrome Abyss</Link>
+               <span aria-hidden="true">/</span>
+               <a href="/projects/exp/prism-veil">Prism Veil</a>
+               <span aria-hidden="true">/</span>
+               <a href="/projects/exp/gridscape">Gridscape</a>
+               <span aria-hidden="true">/</span>
+               <a href="/projects/exp/ironhill">ironhill</a>
+               <span aria-hidden="true">/</span>
+               <Link href="/projects/exp/scroll-expand">Scroll Expand</Link>
+            </ul>
          </main>
       </LayoutHome>
    );
